512c98eecb86dd662d9305e5fddc13a3980f3195
BennettHoffert27 <824540@mystma.org>
2026-03-27 14:32:51 -0500

    March 27

---
+++ Assignments/Animal_Survey.py
@@ -0,0 +1,17 @@
+favorite_animal = input("What is your favorite animal? ->")
+
+what_does_it_eat = input("What does it eat?")
+
+where_does_it_live = input("Where does it live?")
+
+why_do_you_like_this_animal = input("Why do you like this animal?")
+
+what_color_is_it = input("What color is it?")
+
+how_big_is_it = input("How big is it?")
+
+print("Thank you! Your favorite animal is a" + favorite_animal + "." + " " + "It lives in" + where_does_it_live + "." + " " + "It eats" + what_does_it_eat + "." + " " + "You like this animal because " + why_do_you_like_this_animal + "." + " " + "It's color is" + what_color_is_it + "." + " " +  " This animal is" + how_big_is_it + ".")
+
+
+
+
---
+++ Practice/2.3.py
@@ -0,0 +1,19 @@
+countdown = (input("How many seconds until launch?"))
+
+oxygen_tanks = (input("How many oxygen tanks available for mission?"))
+
+food_packs = (input("How many food packs available for mission?"))
+
+water_packs = (input("How many water packs stored on ship?"))
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks: "+ oxygen_tanks)
+print("Food Packs: " + food_packs)
+print("Water Packs: " + water_packs)
+
+oxygen_tanks = (input("Reconfirm Oxygen Tank Number?"))
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks: " + oxygen_tanks)
+print("Food Packs: " + food_packs)
+print("Water Packs: " +  water_packs)
