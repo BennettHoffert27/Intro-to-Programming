068ffcf9fef9f32329dcd465ff7783123b0ee315
BennettHoffert27 <824540@mystma.org>
2026-04-07 11:00:54 -0500

    Simple Mad Lib

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,34 @@
+# As much as the world needs [plural noun],
+# it also needs [noun]. And what could be [adjective]
+# than... [noun]? Yes, [same noun].
+# It would cause the most important study - otherwise known as [study] - to be
+# completely [adjective]. Do you agree? Is tttttttttttttthis [boolean value]?
+# Or do we need to go to [place] to resolve the... IH! IH! IH! IH! [NOUN]?!
+
+# First, prompt the user for a plural noun.
+# Next, prompt the user for a noun.
+# Line 3: prompt the user for a noun and then that same noun.
+# Line 4: Prompt the user to input an "ism" term.
+# Line 5: Prompt the user to input an adjective, and then a boolean value (true or false).
+# Line 6: Prompt the user to input a place and then an ALL_CAPS NOUN.
+
+
+plurnoun = input("Plural Noun? ")
+
+noun_1 = input("Noun? ")
+
+adjective_1 = input("Adjective? ")
+
+noun_2 = input("Seconnd Noun? ")
+
+study = input("Study? ")
+
+adjective_2 = input("Second Adjective? ")
+
+boolean_value = input("'True' or 'False'? ")
+
+place = input("Place? ")
+
+all_caps_noun = input("NOUN IN ALL CAPS? ")
+
+print("As much as the world needs" + "" + plurnoun + "," + "" + "it also needs" + noun_1 + "." + "" + "And what could be" + adjective_1 + " than" + noun_2 + "?" + "" + "Yes, " + noun_2 + "." + "It would cause the most important study - otherwise known as " + study + " - to be completely" +adjective_2 + "." + "" + "Do you agree? " + "Is tttttttttttttthis" + boolean_value + "?" + "" + "Or do we need to go to" + place + "to resolve the...IH! IH! IH! IH!" + all_caps_noun + "?!")
