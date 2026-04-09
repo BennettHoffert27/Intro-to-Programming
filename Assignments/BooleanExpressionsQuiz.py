6150f099077a784a1831bf71f386ca0199ca2f77
BennettHoffert27 <824540@mystma.org>
2026-04-09 14:15:40 -0500

    Completed Boolean Questions Assignment

---
+++ Assignments/BooleanExpressionsQuiz.py
@@ -0,0 +1,14 @@
+question_1 = int(input("Give a whole number less than 3. "))
+print(int(question_1) < 3)
+
+question_2 = int(input("Give a whole number greater than 67. "))
+print(int(question_2) > 67)
+
+question_3 = int(input("Give a whole number greater or equal to 89."))
+print(int(question_3) >= 67)
+
+question_4 = int(input("Give a whole number not equal to 12. "))
+print(int(question_4) != 12)
+
+question_5 = int(input("Give a whole number that IS equal to 12. "))
+print(int(question_5) == 12)
